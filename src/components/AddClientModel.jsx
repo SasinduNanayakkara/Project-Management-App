dbc13c0f24d6e0574cd3a66bc66b1d97f2bb1bf7
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-08-20 15:26:55 +0530

    Implement add client modal

--- src/components/AddClientModel.jsx
+++ src/components/AddClientModel.jsx
@@ -1,55 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaUser } from "react-icons/fa";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import { useMutation } from "@apollo/client";
+import { ADD_CLIENT } from "../mutations/clientMutations";
+import {GET_CLIENTS} from "../queries/clientQuery";
+
 const AddClientModel = () => {
+
+  const [show, setShow] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const [addClient] = useMutation(ADD_CLIENT, {
+    variables: {name, email, phone},
+    update(cache, { data: { addClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: { clients: [...clients, addClient] },
+      });
+    }
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (name === "" || email === "" || phone === "") {
+      return alert("Please fill all the fields");
+    }
+    addClient(name, email, phone);
+    setName("");
+    setEmail("");
+    setPhone("");
+    setShow(false);
+  }
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
-      <button
-        type="button"
-        className="btn btn-secondary"
-        data-toggle="modal"
-        data-target="addClientModal"
-      >
-        <div className="d-flex align-items-center">
-            <FaUser className="icon"/> Add Client
-        </div>
-      </button>
+      <Button variant="primary" onClick={handleShow}>
+        <FaUser /> Add Client
+      </Button>
 
-      <div
-        className="modal fade"
-        id="addClientModal"
-        role="dialog"
-        aria-labelledby="addClientModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="addClientModalLabel">
-                Add Client
-              </h5>
-              <button
-                type="button"
-                className="close"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Client</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form>
+            <div className="mb-3">
+              <label className="form-label">Name</label>
+              <input 
+                type="text" 
+                className="form-control" 
+                id="name" 
+                value={name} 
+                onChange={(e) => setName(e.target.value)} />
             </div>
-            <div className="modal-body">
-                <form>
-                    <div className="mb-3">
-                        <label className="form-label">
-                            Name
-                        </label>
-                        <input type="text" className="from-control" id="name" />
-                    </div>
-                </form>
+            <div className="mb-3">
+              <label className="form-label">Email</label>
+              <input 
+                type="email" 
+                className="form-control" 
+                id="email" 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)} />
             </div>
-          </div>
-        </div>
-      </div>
+            <div className="mb-3">
+              <label className="form-label">Phone</label>
+              <input 
+                type="text" 
+                className="form-control" 
+                id="phone" 
+                value={phone} 
+                onChange={(e) => setPhone(e.target.value)} />
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
